fe2c823cd7e2b40fcfce53748a576d94a37a602d
sagi-viktor <sagi.viktor1@gmail.com>
2022-12-13 13:03:18 +0100

    init: project

---
+++ wam/src/test/java/com/petp/wam/WamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petp.wam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

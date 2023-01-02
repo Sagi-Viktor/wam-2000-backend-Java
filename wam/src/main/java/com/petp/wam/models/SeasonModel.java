012b34bc812b151434f223f0d04174597ffc578b
sagi-viktor <sagi.viktor1@gmail.com>
2023-01-02 22:52:11 +0100

    chore: add remove unique from SeasonModel

--- wam/src/main/java/com/petp/wam/models/SeasonModel.java
+++ wam/src/main/java/com/petp/wam/models/SeasonModel.java
@@ -1,15 +1,20 @@
 package com.petp.wam.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Builder
+@Getter
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class SeasonModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String name;
 }

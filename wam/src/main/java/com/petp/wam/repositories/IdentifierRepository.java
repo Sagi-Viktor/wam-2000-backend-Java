0f413ea61d1d492c699c04a27331d69b81a3fddf
sagi-viktor <sagi.viktor1@gmail.com>
2023-01-05 11:02:59 +0100

    chore: rename IdentifierModel.java to TicketModel.java & IdentifierRepository.java to TicketRepository.java

--- wam/src/main/java/com/petp/wam/repositories/IdentifierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.petp.wam.repositories;
-
-import com.petp.wam.models.IdentifierModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IdentifierRepository extends JpaRepository<IdentifierModel, Long> {
-}
